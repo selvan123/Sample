208e56abf2e94860f46b4d068214804960fa7ea8
S <selvan.s@capgemini.com>
2017-07-11 13:46:49 +0530

    Signed-off-by: S <selvan.s@capgemini.com>

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -10,7 +10,7 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         BL.Class1 obj = new BL.Class1();
-        obj.Add(1, 5);
+        obj.Add(1, 7);
 
     }
 }
